f81d78e6deb4a4d3a86f709c49e63bf4c716c5d5
yappynoppy <hyano1991@gmail.com>
2019-01-16 00:31:02 +0900

    move left menu together

--- TweetDeckMoveColumnToHead.js
+++ TweetDeckMoveColumnToHead.js
@@ -1,6 +1,8 @@
 javascript:(
     function(){
-      var container = document.getElementById("container");
-      container.lastElementChild.insertBefore(container.lastElementChild.lastElementChild, container.lastElementChild.firstChild);
+      var container_child = document.getElementById("container").firstElementChild;
+      container_child.insertBefore(container_child.lastElementChild, container_child.firstChild);
+      var column_navi_grandson= document.getElementById("column-navigator").firstElementChild.firstElementChild;
+      column_navi_grandson.insertBefore(column_navi_grandson.lastElementChild, column_navi_grandson.firstChild);
     }
   )();
